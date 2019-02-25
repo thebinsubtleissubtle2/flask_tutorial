fb5516974de70cdf14658b4d5a7ce55a0231d86a
RC Pery <rcpery@tutanota.com>
2019-02-25 19:22:00 +0800

    added email

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
+from flask_login import UserMixin
+from . import db, login_manager
 
 
 class Role(db.Model):
@@ -15,7 +16,8 @@ class Role(db.Model):
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique = True, index = True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
 
@@ -32,3 +34,7 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
