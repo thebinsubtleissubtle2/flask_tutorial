6405f99a82b9f471ff14e5290422e837311b2dfd
RC Pery <rcpery@tutanota.com>
2019-03-02 21:03:30 +0800

    fixed not sending email

--- flasky.py
+++ flasky.py
@@ -3,17 +3,18 @@ from flask_migrate import Migrate
 from app import create_app, db
 from app.models import User, Role
 
-app = create_app(os.getenv("FLASK_CONFIG") or "default")
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, user=User, role=Role)
+    return dict(db=db, User=User, Role=Role)
+
 
 @app.cli.command()
 def test():
-    """ Run the unit tests """
+    """Run the unit tests."""
     import unittest
-    tests = unittest.TestLoader().discover("tests")
+    tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
