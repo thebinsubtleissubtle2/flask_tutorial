eb351251c1c909921109b1ea912e80df71d88755
RC Pery <rcpery@tutanota.com>
2019-03-03 13:42:50 +0800

    user roles and permissions

--- app/main/errors.py
+++ app/main/errors.py
@@ -3,6 +3,10 @@ from . import main
 
 
 @main.app_errorhandler(404)
+def forbidden(e):
+    return render_template('403.html'), 403
+
+@main.app_errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
